a60b919a5d101498a09446f77ea991abaf97f1bc
miguel <miguelangelgalan@gmail.com>
2025-08-09 10:48:07 +0200

    Growing Sprite when tapping

--- lib/Player.dart
+++ lib/Player.dart
@@ -1,11 +1,17 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 
-class Player extends SpriteComponent {
+class Player extends SpriteComponent with TapCallbacks {
   Player({super.position})
     : super(size: Vector2.all(200), anchor: Anchor.center);
 
   @override
   Future<void> onLoad() async {
     sprite = await Sprite.load('Player.png');
+  }
+
+  @override
+  void onTapUp(TapUpEvent info) {
+    size += Vector2.all(50);
   }
 }
