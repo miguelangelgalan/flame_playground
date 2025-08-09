4fc2a7c5e52ace3effd62b70e3ca266e7e7aad90
miguel <miguelangelgalan@gmail.com>
2025-08-09 10:39:54 +0200

    Loading Sprite

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flame/game.dart';
+import 'package:flame_playground/Player.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/components.dart';
 
 void main() {
-  final game = FlameGame();
+  final game = FlameGame(world: MyWorld());
   runApp(GameWidget(game: game));
+}
+
+class MyWorld extends World {
+  @override
+  Future<void> onLoad() async {
+    add(Player(position: Vector2(0, 0)));
+  }
 }
